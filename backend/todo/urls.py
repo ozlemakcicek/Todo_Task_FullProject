8415c931a9ed9c9237251c6c16b7c5d0f3bf6055
Özlem AKCICEK <122410775+ozlemakcicek@users.noreply.github.com>
2023-11-11 00:19:34 +0100

    Urls update

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -3,6 +3,6 @@ from .views import todo_list_create, todo_get_delete_update, task_groups, task_g
 urlpatterns = [
     path("api/todo/", todo_list_create),
     path("api/todo/<int:pk>", todo_get_delete_update),
-    path("api/groups", task_groups),
+    path("api/groups/", task_groups),
     path("api/groups/<int:pk>", task_group_get_update_delete),
 ]
